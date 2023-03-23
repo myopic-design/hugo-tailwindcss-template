5d177ddd041d8ac64eac9c68cd64f73ecafd9fdd
Daniel Perez Alvarez <daniel@unindented.org>
2023-03-22 20:40:52 -0700

    fix: make all paths relative

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,7 @@
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("{{ $serviceWorker.RelPermalink }}", {
-    scope: "/hugo-tailwindcss-template/",
+    scope: "{{ "" | relURL }}",
   });
 
   window.addEventListener("load", () => {
