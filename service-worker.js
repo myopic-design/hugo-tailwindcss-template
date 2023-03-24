4f1588cb41cd7a9137898d5ebe4502616e7e7b5a
unindented <unindented@users.noreply.github.com>
2023-03-24 00:26:02 +0000

    deploy: d953ce1668ce4469f38389d84c78ea11433b8074

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 
 */
 
-const version = "v20230323";
+const version = "v20230324";
 const staticCacheName = version + "-static";
 const pagesCacheName = version + "-pages";
 const imagesCacheName = version + "-images";
